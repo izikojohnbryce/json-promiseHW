d9266793524f4aca95f8533e00be5bc77ca8a198
izikojohnbryce <izikosher@gmail.com>
2020-07-16 00:49:09 +0300

    work

--- func.js
+++ func.js
@@ -18,3 +18,33 @@ let myPromise = () => new Promise((resolve, reject) =>{
         }
     });
 });
+
+
+
+
+$("#btn").click(() => myPromise()
+
+.then(response => {
+    return {
+        stauts:100, // adding ineer status for the "then" exm. i we want to use in internal status.
+        errorMessage:"",
+        data:response}
+}).catch(error=>{
+
+    return {
+        stauts:1000,
+        errorMessage:error,
+        data:[]
+    }
+})
+
+.then((res) => {
+    const html = res.data.map(value=>
+            `<div>
+                <h2>Country Name:${value.name}</h2>
+                <img src="${value.flag}" width="300px" height="100px" />
+                <p>Population:${value.count}</p>
+            </div>`
+    ).join('');
+    $('#country').html(html)
+}))
